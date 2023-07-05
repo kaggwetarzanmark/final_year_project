4a9c9dadd7d6a0f6b06bb27a793f678e23e9bc74
Nakuya Shamimuh <shamimuhnakuya0@gmail.com>
2023-07-05 16:14:44 +0300

    report

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -7,34 +7,46 @@ const connection = mysql.createConnection({
   password: '',
   database: 'supermarket',
 });
+
 function renderReportsPage(req, res) {
-  if (req.session.user && req.session.user.user_type === "Sales Manager") {
-    connection.query('SELECT salesdata_id, quantity, date, product.product_name FROM salesorder JOIN product ON salesorder.product_id = product.product_id', (error, results) => {
-      if (error) {
-        // Handle the error
-        console.error(error);
-        return;
-      }
-      
-      const salesOrders = results.map((order) => {
-        return {
-          ...order,
-          date: new Date(order.date).toLocaleDateString('en-US', {
-            weekday: 'short',
-            month: 'short',
-            day: '2-digit',
-            year: 'numeric',
-          }),
-        };
-      });
+  if (req.session.user && req.session.user.user_type === 'Sales Manager') {
+    const query = `
+    SELECT product.product_name, SUM(salesorder.quantity) AS total_quantity, product.available_quantity
+    FROM salesorder
+    JOIN product ON salesorder.product_id = product.product_id
+    GROUP BY product.product_name, product.available_quantity`;
   
-      res.render('reports', { salesOrders: salesOrders });
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send('Error retrieving sales data');
+      }
+
+      const salesOrders = results.map((order) => ({
+        Quantity: order.Quantity,
+        Total: order.Total,
+        product_name: order.product_name,
+        available_quantity: order.available_quantity,
+      }));
+
+      const recordsPerPage = 10;
+      const currentPage = req.query.page || 1;
+      const startIndex = (currentPage - 1) * recordsPerPage;
+      const endIndex = startIndex + recordsPerPage;
+      const paginatedOrders = salesOrders.slice(startIndex, endIndex);
+
+      res.render('reports', {
+        salesOrders: salesOrders,
+        paginatedOrders: paginatedOrders,
+        currentPage: currentPage,
+        recordsPerPage: recordsPerPage,
+      });
     });
   } else {
-    res.redirect("/");
+    res.redirect('/');
   }
-  }
-  
-  module.exports = {
-    renderReportsPage,
-  };
+}
+
+module.exports = {
+  renderReportsPage,
+};
