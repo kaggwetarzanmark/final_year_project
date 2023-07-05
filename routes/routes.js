bddf9cc34970ad207bdd4ecec7463a369d0d1306
Nakuya Shamimuh <shamimuhnakuya0@gmail.com>
2023-07-05 16:19:30 +0300

    report

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,12 @@ const reportsController = require('../controllers/reportsController');
 const addController = require('../controllers/addController');
 const ForecastingController = require('../controllers/ForecastingController');
 const { isAuthenticated } = require('./auth');
+<<<<<<< Updated upstream
 const forecastController = require('../controllers/forecastController')
 
+=======
+const dbConfig = require('./db_config');
+>>>>>>> Stashed changes
 
 router.get(['/', '/login'], loginController.renderLoginPage);
 router.get('/register', registerController.renderRegisterPage);
@@ -15,9 +19,38 @@ router.get('/reports', isAuthenticated, reportsController.renderReportsPage);
 router.get('/add', isAuthenticated, addController.renderaddPage);
 router.get('/forecasting', isAuthenticated, ForecastingController.renderforecastingPage);
 router.post('/register', registerController.registerUser);
+<<<<<<< Updated upstream
 router.post('/stock', addController.addStock);
 router.post('/sales', isAuthenticated, addController.createSalesOrder);
 router.get('/forecasts', forecastController.forecast)
 
 
+=======
+router.get('/sales', (req, res) => {
+    // Create a MySQL connection using the dbConfig.createConnection function
+    const connection = dbConfig.createConnection();
+  
+    // Query the database to fetch product and sales order data
+    const query = `
+      SELECT p.product_name, p.price, p.available_quantity, s.Date, s.quantity, s.temperature, s.competitorPrice, s.competitorCount, s.advertisingCost, s.promotion
+      FROM product p
+      INNER JOIN salesorder s ON p.product_id = s.product_id
+    `;
+    connection.query(query, (error, data) => {
+      if (error) {
+        // Handle database query error
+        console.error('Error retrieving data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        // Send the JSON response with product and sales order data
+        console.log('Data retrieved successfully:', data);
+        res.json(data);
+      }
+  
+      // Close the MySQL connection
+      connection.end();
+    });
+  });
+  
+>>>>>>> Stashed changes
 module.exports = router;
