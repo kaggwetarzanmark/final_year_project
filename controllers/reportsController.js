51b80ae584053ea966981141ab169c954369eead
Iprogrammer26 <64302116+Iprogrammer26@users.noreply.github.com>
2023-07-05 19:50:00 -0700

    Reports Dashboard

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -17,17 +17,17 @@ function renderReportsPage(req, res) {
     GROUP BY product.product_name, product.available_quantity`;
   
     connection.query(query, (error, results) => {
-      if (error) {
-        console.error(error);
+        if (error) {
+          console.error(error);
         return res.status(500).send('Error retrieving sales data');
-      }
+        }
 
-      const salesOrders = results.map((order) => ({
+        const salesOrders = results.map((order) => ({
         Quantity: order.Quantity,
         Total: order.Total,
         product_name: order.product_name,
         available_quantity: order.available_quantity,
-      }));
+        }));
 
       const recordsPerPage = 10;
       const currentPage = req.query.page || 1;
@@ -45,50 +45,40 @@ function renderReportsPage(req, res) {
   } else {
     res.redirect('/');
   }
+}
 
+
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+function graphdata(req, res) {
+  const productName = req.params.product_name;
+
+  const query = `
+    SELECT MONTH(salesorder.date) AS month, SUM(salesorder.quantity) AS totalQuantity
+    FROM salesorder
+    JOIN product ON salesorder.product_id = product.product_id
+    WHERE product.product_name = ?
+    GROUP BY MONTH(salesorder.date)
+    ORDER BY MONTH(salesorder.date)
+  `;
+
+  connection.query(query, [productName], (error, results) => {
+    if (error) {
+      console.error('Error executing MySQL query:', error);
+      return res.status(500).json({ error: 'An error occurred' });
+    }
+
+    const graphData = results.map((row) => ({
+      month: monthNames[row.month - 1],  // Subtract 1 to match the array index
+      totalQuantity: row.totalQuantity,
+    }));
+
+    res.json(graphData);
+  });
 }
 
 module.exports = {
   renderReportsPage,
+  graphdata,
 };
-
-
-  function graphdata(req, res) {
-    connection.query(`
-      SELECT product.product_name, WEEK(salesorder.date) AS week, SUM(salesorder.quantity) AS weekly_sales, 
-             MONTH(salesorder.date) AS month, SUM(salesorder.quantity) AS monthly_sales
-      FROM salesorder
-      JOIN product ON salesorder.product_id = product.product_id
-      GROUP BY product.product_name, WEEK(salesorder.date), MONTH(salesorder.date)
-    `, (error, results) => {
-      if (error) {
-        // Handle the error
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-        return;
-      }
-  
-      try {
-        const salesOrders = results.map((order) => {
-          return {
-            productName: order.product_name,
-            weeklySales: order.weekly_sales,
-            monthlySales: order.monthly_sales,
-          };
-        });
-  
-        res.json(salesOrders);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
-      }
-    });
-  }
-  
-
-
-  
-  module.exports = {
-    renderReportsPage,graphdata
-  };
-
